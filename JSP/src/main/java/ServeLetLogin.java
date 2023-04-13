d00d776442d31b3043b56f1a2d7478c21cbac278
03593241188 <03593241188@L04001017425.edu.senac.br>
2023-04-12 21:30:43 -0300

    Primeiro commit do projeto

---
+++ JSP/src/main/java/ServeLetLogin.java
@@ -0,0 +1,47 @@
+																																																																																																				
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class ServeLetLogin
+ */
+public class ServeLetLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServeLetLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+		request.setAttribute("msg", "Informe o login e senha corretamente");
+		
+	}
+
+}
---
+++ JSP/src/main/webapp/index.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1>Projeto em JSP</h1>
+
+<form action="ServLetLogin" method="post">
+<table>
+	<tr>
+		<td>Login</td>
+		<td><input name="Login" type="text"></td>
+	</tr>
+	
+	<tr>
+		<td>Senha</td>
+		<td><input name="Senha" type="password"></td>
+	</tr>
+	<tr>
+		<td><input type="submit" value="Enviar"></td>
+	</tr>
+
+</table>
+</form>
+
+
+
+
+
+
+</body>
+</html>
